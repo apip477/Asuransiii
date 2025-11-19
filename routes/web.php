70cdf3e690186b938e245f6d19cc83a8ddce1b02
rifkyputra158-maker <rifkyputra158@gmail.com>
2025-11-19 14:50:12 +0700

    menambahkan about us

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,10 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
+Route::get('/about-us', function () {
+    return view('about');
+})->name('about');
+
 // 2. JALUR KHUSUS (Hanya Member yang Login)
 Route::get('/dashboard', function () {
     return view('dashboard');
